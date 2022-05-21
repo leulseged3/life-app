cf39d2476364775d84183894fb6444f489cf16d8
leulseged3 <leulseged3@gmail.com>
2022-05-21 14:18:13 +0300

    ✨create end user model and migration

---
+++ app/Models/Enduser.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+
+class Enduser extends Model
+{
+    use HasFactory,Notifiable,HasApiTokens;
+
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'username',
+        'mobile_number',
+        'email',
+        'is_mhp',
+        'password',
+    ];
+
+     /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
