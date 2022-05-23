26276a8772cf34fe1a61806cdc28c99b489cba6c
leulseged3 <leulseged3@gmail.com>
2022-05-23 13:11:27 +0300

    🩹 retrieve only non mhp users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,7 @@ use App\Models\Enduser;
 class UserController extends Controller
 {
     function index(){
-        $users = Enduser::all();
+        $users = Enduser::where('is_mhp',0)->get();
         return view('users.index')->with('users',$users);
     }
 
