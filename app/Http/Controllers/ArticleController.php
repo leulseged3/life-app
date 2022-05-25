630405515a0632507514612eb3b687937a75f28b
leulseged3 <leulseged3@gmail.com>
2022-05-25 17:25:16 +0300

    👔 add create article functionality

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,10 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+use App\Models\Article;
 
 class ArticleController extends Controller
 {
     public function index(){
-        return view('articles.index');
+        $articles = Article::all();
+
+        return view('articles.index')->with('articles', $articles);
+    }
+
+    public function create(Request $request){
+        Validator::make($request->all(), [
+            'title' => 'required|string',
+            'description' => 'required',
+            'feature_image' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
+            'video_link' => 'string',
+            "categories"    => "required|array|min:1",
+            "categories.*"  => "required|string|distinct|min:1",
+        ])->validate();
+
+        $path = $request->file('feature_image')->store('public/feature_images');
+        $icon_name = explode("/", $path)[2];
+
+        $article = new Article;
+        $article->title = $request->title;
+        $article->description = $request->description;
+        $article->feature_image = $icon_name;
+        $article->video_link = $request->video_link;
+        $article->save();
+        $article->categories()->attach($request->categories);
+        return view('articles.add')->with('message','successfully create article');
     }
 }
