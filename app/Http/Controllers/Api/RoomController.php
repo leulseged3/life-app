489f1f13e179dfcca54ee45c69c51d58f7dd0754
leulseged3 <leulseged3@gmail.com>
2022-05-30 14:20:21 +0300

    👔 create add new room controller and route

---
+++ app/Http/Controllers/Api/RoomController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Room;
+
+class RoomController extends Controller
+{
+    public function create(Request $request){
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:255',
+            'limit' => 'required|numeric',
+            'date_time' => 'required|string|max:255',
+            "categories"    => "required|array|min:1",
+            "categories.*"  => "required|string|distinct|min:1",
+        ]);
+
+        $room = new Room;
+        $room->title = $request->title;
+        $room->user_id = $request->user()->id;
+        $room->limit = $request->limit;
+        $room->date_time = $request->date_time;
+
+        $room->save();
+        $room->categories()->attach($request->categories);
+
+        return response()->json([
+            'title' => $room->title,
+            'limit' => $room->limit,
+            'date_time' => $room->date_time,
+            'categories' => $room->categories
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\RoomController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->prefix('rooms')->group(function() {
+    Route::post('/create', [RoomController::class, 'create']);
+});
