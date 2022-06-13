a164ae9a2b9c45ebf85928f16950f376198675d2
leulseged3 <leulseged3@gmail.com>
2022-06-13 13:56:53 +0300

    🎨 add email validation

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,5 +1,4 @@
 <x-layouts.auth>
-  <p class="login-box-msg">Reset Password</p>
   @if (session('status'))
     {{-- <div class="mb-4 font-medium text-sm text-green-600">
         {{ session('status') }}
@@ -10,19 +9,31 @@
   @endif
   <form action="forgot-password" method="post">
     @csrf
-    <div class="input-group mb-3">
-      <input type="email" class="form-control" placeholder="Email" name="email">
-      <div class="input-group-append">
-        <div class="input-group-text">
-          <span class="fas fa-envelope"></span>
+    <div class="card-body">
+      <p class="login-box-msg">You forgot your password? Here you can easily retrieve a new password.</p>
+      <div class="input-group mb-3">
+        <input 
+          type="email" 
+          class="form-control" 
+          placeholder="Email" 
+          name="email"
+          pattern="[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}"
+          required
+        >
+        <div class="input-group-append">
+          <div class="input-group-text">
+            <span class="fas fa-envelope"></span>
+          </div>
         </div>
       </div>
-    </div>
-
-    <div class="row">
-      <div class="col-4">
-        <button type="submit" class="btn btn-primary btn-block">Reset</button>
+      <div class="row">
+        <div class="col-12">
+          <button type="submit" class="btn btn-primary btn-block">Request new password</button>
+        </div>
       </div>
+      <p class="mt-3 mb-1">
+        <a href="/login">Login</a>
+      </p>
     </div>
   </form>
   @foreach ($errors->all() as $error)
