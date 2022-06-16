8a6fd87a7ea9c4665834ffc0c122f259eef536e7
leulseged3 <leulseged3@gmail.com>
2022-06-16 14:41:07 +0300

    💄 ui modification to side bar profile pic

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -100,19 +100,21 @@ to get the desired effect
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex" style="align-items: center">
         <div class="image">
           @if (Auth::user()->profile_pic)
             <img 
               class="img-circle elevation-2"
               src="{{ URL::asset('storage/profile_pics/'.Auth::user()->profile_pic) }}" 
               alt="User profile picture"
+              style="width: 30px; height: 30px;"
             >
           @else
             <img 
               class="img-circle elevation-2"
               src="{{ URL::asset('img/profile_avatar.png') }}" 
               alt="User profile picture"
+              style="width: 30px; height: 30px;"
             >
           @endif
         </div>
@@ -122,16 +124,7 @@ to get the desired effect
       </div>
 
       <!-- SidebarSearch Form -->
-      <div class="form-inline">
-        <div class="input-group" data-widget="sidebar-search">
-          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-          <div class="input-group-append">
-            <button class="btn btn-sidebar">
-              <i class="fas fa-search fa-fw"></i>
-            </button>
-          </div>
-        </div>
-      </div>
+     
 
       <!-- Sidebar Menu -->
       <nav class="mt-2">
