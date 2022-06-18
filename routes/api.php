e8af40627b72716195c6cf13a8de934e7de42d57
leulseged3 <leulseged3@gmail.com>
2022-06-18 23:19:06 +0300

    👔 add logging for api

--- routes/api.php
+++ routes/api.php
@@ -28,7 +28,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/register', [AuthController::class, 'register']);
+Route::post('/register', [AuthController::class, 'register'])->middleware('log.route');
 Route::post('/login', [AuthController::class, 'login']);
 
 //ROOMS ROUTES
