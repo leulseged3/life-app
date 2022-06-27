159532f126d2fe4bdb7048c027a6cfbd14f00e07
leulseged3 <leulseged3@gmail.com>
2022-06-27 13:59:12 +0300

    🗃️ role model and schema

---
+++ app/Models/Role.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+
+    /**
+     * The roles that belong to the Role
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+}
