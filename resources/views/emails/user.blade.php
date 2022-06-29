59ac3f0ffb382b2cc0ed76dddceb756767eaf5a0
leulseged3 <leulseged3@gmail.com>
2022-06-29 16:08:57 +0300

    ✨ user comfirmation mailer

---
+++ resources/views/emails/user.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Verify Email</title>
+</head>
+
+<body>
+<h2>Dear {{$info['name']}},</h2><br/>
+Your Verification Code is <b>{{$info['verification']}}</b>
+</body>
+
+</html>
