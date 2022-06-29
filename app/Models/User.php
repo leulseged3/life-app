8feec6006aee8bfb6927f9de643b22260cc5ff0f
leulseged3 <leulseged3@gmail.com>
2022-06-29 16:08:21 +0300

    🗃️ email verification model, schema & relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -69,6 +69,10 @@ class User extends Authenticatable
         return $this->belongsToMany(Speciality::class);
    }
 
+   public function verifications(){
+    return $this->hasMany(Verification::class);
+   }
+
    /**
     * The roles that belong to the User
     *
