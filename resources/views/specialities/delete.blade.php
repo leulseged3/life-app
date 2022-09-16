2f148753ab3c1151e6373c82c97d8898ed5123ca
leulseged3 <leulseged3@gmail.com>
2022-09-16 23:19:13 +0300

    🩹 overflow fix

--- resources/views/specialities/delete.blade.php
+++ resources/views/specialities/delete.blade.php
@@ -2,14 +2,14 @@
   <script>
     $('#speciality-delete-modal').on('show.bs.modal', function(e) {
       var speciality = $(e.relatedTarget).data('speciality');
-      $('#speciality_delete_title').text("Delete " + speciality.title + " Speciality");
+      $('#speciality_delete_title').text("Delete " + speciality.title.substring(0, 35) + " Speciality");
       $('#speciality_delete_id').val(speciality.id);
     });
   </script>
 @endpush
 <div class="modal fade" id="speciality-delete-modal">
   <div class="modal-dialog">
-    <div class="modal-content">
+    <div class="modal-content" style="overflow-x: hidden;">
       <div class="modal-header">
         <h4 class="modal-title" id="speciality_delete_title">Delete Speciality</h4>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
